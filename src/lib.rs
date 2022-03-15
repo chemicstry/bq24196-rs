74fd4e5c2022622e5810793076f97c5a01191db3
chemicstry <chemicstry@gmail.com>
2022-03-15 16:13:36 +0200

    Basic functionality

--- src/lib.rs
+++ src/lib.rs
@@ -5,135 +5,125 @@ mod registers;
 
 use embedded_hal::blocking::i2c::{Write, WriteRead};
 use registers::I2cRegister;
-use tock_registers::{register_bitfields, register_bitmasks, fields::Field, RegisterLongName, LocalRegisterCopy};
+use tock_registers::LocalRegisterCopy;
 
-const ADDR: u8 = 0x6B;
+const DEVICE_ADDR: u8 = 0x6B;
+
+pub use registers::{
+    REG00::{IINLIM::Value as InputCurrentLimit, VINDPM::Value as InputVoltageLimit},
+    REG08::{CHRG_STAT::Value as ChargerStatus, VBUS_STAT::Value as VBusStatus},
+};
+
+#[derive(Debug)]
+pub enum Error<E> {
+    /// I2C bus error
+    Bus(E),
+    /// Device P/N did not match during initialization
+    UnknownDevice(u8),
+}
 
 pub struct BQ24196<I2C> {
-    i2c: I2C
+    i2c: I2C,
 }
-
-pub enum ChargerFault {
-    /// Input fault (VBUS OVP or VBAT < VBUS < 3.8 V)
-    InputFault,
-    /// Thermal shutdown
-    ThermalShutdown,
-    /// Charge Safety Timer Expiration
-    SafetyTimer,
-}
-
-pub enum NtcFault {
-    /// Battery too cold
-    Cold,
-    /// Battery too hot
-    Hot,
-}
-
-pub struct FaultRegister {
-    val: u8,
-}
-
-impl FaultRegister {
-    const ADDR: u8 = 0x09;
-
-    /// Watchdog timer expiration
-    pub fn watchdog_expired(&self) -> bool {
-        self.val & 0b1000_0000 != 0
-    }
-
-    /// VBUS overloaded (OCP), or VBUS OVP in boost mode
-    pub fn boost_fault(&self) -> bool {
-        self.val & 0b0100_0000 != 0
-    }
-
-    /// Charger fault
-    pub fn charger_fault(&self) -> Option<ChargerFault> {
-        match self.val & 0b0011_0000 >> 4 {
-            0b01 => Some(ChargerFault::InputFault),
-            0b10 => Some(ChargerFault::ThermalShutdown),
-            0b11 => Some(ChargerFault::SafetyTimer),
-            _ => None,
-        }
-    }
-
-    /// Battery overvoltage
-    pub fn battery_fault(&self) -> bool {
-        self.val & 0b0000_1000 != 0
-    }
-
-    /// Battery temperature fault
-    pub fn ntc_fault(&self) -> Option<NtcFault> {
-        match self.val & 0b0000_0111 {
-            0b101 => Some(NtcFault::Cold),
-            0b110 => Some(NtcFault::Hot),
-            _ => None,
-        }
-    }
-}
-
-pub use registers::InputSourceControl::{
-    InputVoltageLimit::Value as InputVoltageLimit,
-    InputCurrentLimit::Value as InputCurrentLimit,
-};
 
 impl<I2C, E> BQ24196<I2C>
 where
     I2C: WriteRead<Error = E> + Write<Error = E>,
 {
-    // pub fn read_faults(&mut self) -> Result<FaultRegister, E> {
-    //     Ok(FaultRegister { val: self.read_reg(FaultRegister::ADDR)? })
-    // }
+    pub fn new(i2c: I2C) -> Result<Self, Error<E>> {
+        let mut dev = Self { i2c };
 
-    pub fn get_input_voltage_limit(&mut self) -> Result<InputVoltageLimit, E> {
-        Ok(self.read_reg()?.read_as_enum(registers::InputSourceControl::InputVoltageLimit).unwrap())
+        let pn = dev.read_reg()?.read(registers::REG0A::PN);
+        if pn != registers::REG0A::PN::Value::DEFAULT_PN as _ {
+            return Err(Error::UnknownDevice(pn));
+        }
+
+        Ok(dev)
     }
 
-    pub fn set_input_voltage_limit(&mut self, lim: InputVoltageLimit) -> Result<(), E> {
+    pub fn is_input_high_z(&mut self) -> Result<bool, Error<E>> {
+        Ok(self.read_reg()?.is_set(registers::REG00::EN_HIZ))
+    }
+
+    pub fn set_input_high_z(&mut self, en: bool) -> Result<(), Error<E>> {
         self.modify_reg(|reg| {
-            reg.modify(registers::InputSourceControl::InputVoltageLimit.val(lim as _));
+            reg.modify(registers::REG00::EN_HIZ.val(en as _));
         })
     }
 
-    pub fn get_input_current_limit(&mut self) -> Result<InputCurrentLimit, E> {
-        Ok(self.read_reg()?.read_as_enum(registers::InputSourceControl::InputCurrentLimit).unwrap())
+    pub fn get_input_voltage_limit(&mut self) -> Result<InputVoltageLimit, Error<E>> {
+        Ok(self
+            .read_reg()?
+            .read_as_enum(registers::REG00::VINDPM)
+            .unwrap())
     }
 
-    pub fn set_input_current_limit(&mut self, lim: InputCurrentLimit) -> Result<(), E> {
+    pub fn set_input_voltage_limit(&mut self, lim: InputVoltageLimit) -> Result<(), Error<E>> {
         self.modify_reg(|reg| {
-            reg.modify(registers::InputSourceControl::InputCurrentLimit.val(lim as _));
+            reg.modify(registers::REG00::VINDPM.val(lim as _));
         })
+    }
+
+    pub fn get_input_current_limit(&mut self) -> Result<InputCurrentLimit, Error<E>> {
+        Ok(self
+            .read_reg()?
+            .read_as_enum(registers::REG00::IINLIM)
+            .unwrap())
+    }
+
+    pub fn set_input_current_limit(&mut self, lim: InputCurrentLimit) -> Result<(), Error<E>> {
+        self.modify_reg(|reg| {
+            reg.modify(registers::REG00::IINLIM.val(lim as _));
+        })
+    }
+
+    pub fn vbus_status(&mut self) -> Result<VBusStatus, Error<E>> {
+        Ok(self
+            .read_reg()?
+            .read_as_enum(registers::REG08::VBUS_STAT)
+            .unwrap())
+    }
+
+    pub fn charger_status(&mut self) -> Result<ChargerStatus, Error<E>> {
+        Ok(self
+            .read_reg()?
+            .read_as_enum(registers::REG08::CHRG_STAT)
+            .unwrap())
+    }
+
+    /// True if any of the faults are active
+    pub fn has_fault(&mut self) -> Result<bool, Error<E>> {
+        Ok(self.read_reg::<registers::REG09::Register>()?.get() != 0)
     }
 
     #[inline]
-    fn read_reg<REG: I2cRegister>(&mut self) -> Result<LocalRegisterCopy::<u8, REG>, E> {
+    fn read_reg<REG: I2cRegister>(&mut self) -> Result<LocalRegisterCopy<u8, REG>, Error<E>> {
         let mut buf = [0u8];
-        self.i2c.write_read(ADDR, &[REG::ADDR], &mut buf)?;
+        self.i2c
+            .write_read(DEVICE_ADDR, &[REG::ADDR], &mut buf)
+            .map_err(|e| Error::Bus(e))?;
         Ok(LocalRegisterCopy::new(buf[0]))
     }
 
     #[inline]
-    fn write_reg<REG: I2cRegister>(&mut self, reg: LocalRegisterCopy::<u8, REG>) -> Result<(), E> {
-        self.i2c.write(ADDR, &[REG::ADDR, reg.get()])
+    fn write_reg<REG: I2cRegister>(
+        &mut self,
+        reg: LocalRegisterCopy<u8, REG>,
+    ) -> Result<(), Error<E>> {
+        self.i2c
+            .write(DEVICE_ADDR, &[REG::ADDR, reg.get()])
+            .map_err(|e| Error::Bus(e))
     }
 
     #[inline]
-    fn modify_reg<REG, F>(&mut self, f: F) -> Result<(), E>
+    fn modify_reg<REG, F>(&mut self, f: F) -> Result<(), Error<E>>
     where
         REG: I2cRegister,
-        F: FnOnce(&mut LocalRegisterCopy::<u8, REG>),
+        F: FnOnce(&mut LocalRegisterCopy<u8, REG>),
     {
         let mut r = self.read_reg()?;
         f(&mut r);
         self.write_reg(r)?;
         Ok(())
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
     }
 }
--- src/registers.rs
+++ src/registers.rs
@@ -8,9 +8,10 @@ register_bitfields! [
     // First parameter is the register width. Can be u8, u16, u32, or u64.
     u8,
 
-    pub InputSourceControl [
-        HighZ OFFSET(7) NUMBITS(1) [],
-        InputVoltageLimit OFFSET(3) NUMBITS(4) [
+    /// Input Source Control Register
+    pub REG00 [
+        EN_HIZ OFFSET(7) NUMBITS(1) [],
+        VINDPM OFFSET(3) NUMBITS(4) [
             VINDPM_3_88V = 0b0000,
             VINDPM_3_96V = 0b0001,
             VINDPM_4_04V = 0b0010,
@@ -28,7 +29,7 @@ register_bitfields! [
             VINDPM_5_00V = 0b1110,
             VINDPM_5_08V = 0b1111,
         ],
-        InputCurrentLimit OFFSET(0) NUMBITS(3) [
+        IINLIM OFFSET(0) NUMBITS(3) [
             IINLIM_100mA = 0b000,
             IINLIM_150mA = 0b001,
             IINLIM_500mA = 0b010,
@@ -40,77 +41,247 @@ register_bitfields! [
         ]
     ],
 
-    pub PowerOnConfiguration [
+    /// Power-On Configuration Register
+    pub REG01 [
         RegisterReset OFFSET(7) NUMBITS(1) [],
         WatchdogReset OFFSET(6) NUMBITS(1) [],
-        ChargerConfiguration OFFSET(4) NUMBITS(2) [
+        CHG_CONFIG OFFSET(4) NUMBITS(2) [
             Disabled = 0b00,
             ChargeBattery = 0b01,
             Otg = 0b10,
         ],
-        MinSystemVoltageLimit OFFSET(1) NUMBITS(3) [],
-        BoostModeCurrentLimit OFFSET(0) NUMBITS(1) [
+        SYS_MIN OFFSET(1) NUMBITS(3) [],
+        BOOST_LIM OFFSET(0) NUMBITS(1) [
             BOOST_LIM_500mA = 0,
             BOOST_LIM_1300mA = 1,
         ],
     ],
 
-    pub SystemStatus [
-        VBusStatus OFFSET(6) NUMBITS(2) [
+    /// Charge Current Control Register
+    pub REG02 [
+        /// Fast Charge Current Limit
+        ICHG OFFSET(2) NUMBITS(6) [],
+        FORCE_20PCT OFFSET(0) NUMBITS(1) [],
+    ],
+
+    /// Pre-Charge/Termination Current Control Register
+    pub REG03 [
+        /// Pre-Charge Curent Limit
+        IPRECHG OFFSET(4) NUMBITS(4) [],
+        /// Termination Current Limit
+        ITERM OFFSET(0) NUMBITS(4) [],
+    ],
+
+    /// Charge Voltage Control Register
+    pub REG04 [
+        /// Charge Voltage Limit
+        VREG OFFSET(2) NUMBITS(6) [],
+        /// Battery Precharge to Fast Charge Threshold
+        BATLOWV OFFSET(1) NUMBITS(1) [
+            BATLOWV_2_8V = 0,
+            BATLOWV_3_0V = 1,
+        ],
+        /// Battery Recharge Threshold (below battery regulation voltage)
+        VRECHG OFFSET(0) NUMBITS(1) [
+            VRECHG_100mV = 0,
+            VRECHG_300mV = 1,
+        ],
+    ],
+
+    /// Charge Termination/Timer Control Register
+    pub REG05 [
+        /// Charging Termination Enable
+        EN_TERM OFFSET(7) NUMBITS(1) [
+            Disabled = 0,
+            Enabled = 1,
+        ],
+        /// Termination Indicator Threshold
+        TERM_STAT OFFSET(6) NUMBITS(1) [
+            /// STAT pin high when ITERM matches
+            MATCH_ITERM = 0,
+            /// STAT pin high before actual termination when charge current below 800 mA
+            BEFORE_TERM = 1,
+        ],
+        /// I2C Watchdog Timer Setting
+        WATCHDOG OFFSET(4) NUMBITS(2) [
+            Disabled = 0b00,
+            Timer_40s = 0b01,
+            Timer_80s = 0b10,
+            Timer_160s = 0b11,
+        ],
+        /// Charging Safety Timer Enable
+        EN_TIMER OFFSET(3) NUMBITS(1) [
+            Disabled = 0,
+            Enabled = 1,
+        ],
+        /// Fast Charge Timer Setting
+        CHG_TIMER OFFSET(1) NUMBITS(2) [
+            Timer_5h = 0b00,
+            Timer_8h = 0b01,
+            Timer_12h = 0b10,
+            Timer_20h = 0b11,
+        ]
+    ],
+
+    /// Thermal Regulation Control Register
+    pub REG06 [
+        /// Thermal Regulation Threshold
+        TREG OFFSET(0) NUMBITS(2) [
+            TREG_60C = 0b00,
+            TREG_80C = 0b01,
+            TREG_100C = 0b10,
+            TREG_120C = 0b11,
+        ]
+    ],
+
+    /// Misc Operation Control Register
+    pub REG07 [
+        /// Set default input current limit from PSEL/OTG pins
+        DPDM_EN OFFSET(7) NUMBITS(1) [
+            /// Not in D+/D– detection
+            NoDetection = 0,
+            /// Force D+/D– detection
+            ForceDetection = 1,
+        ],
+        /// Safety Timer Setting during Input DPM and Thermal Regulation
+        TMR2X_EN OFFSET(6) NUMBITS(1) [
+            /// Safety timer not slowed by 2X during input DPM or thermal regulation
+            Disabled = 0,
+            /// Safety timer slowed by 2X during input DPM or thermal regulation
+            Enabled = 1,
+        ],
+        /// Force BATFET Off
+        BATFET_Disable OFFSET(5) NUMBITS(1) [
+            AllowQ4On = 0,
+            TurnOffQ4 = 1,
+        ],
+        /// CHRG_FAULT IRT control
+        INT_MASK1 OFFSET(1) NUMBITS(1) [
+            /// No INT during CHRG_FAULT
+            Disabled = 0,
+            /// INT on CHRG_FAULT
+            Enabled = 1,
+        ],
+        /// BAT_FAULT INT control
+        INT_MASK0 OFFSET(0) NUMBITS(1) [
+            /// No INT during BAT_FAULT
+            Disabled = 0,
+            /// INT on BAT_FAULT
+            Enabled = 1,
+        ],
+    ],
+
+    /// System Status Register
+    pub REG08 [
+        VBUS_STAT OFFSET(6) NUMBITS(2) [
             /// Unknown (no input, or DPDM detection incomplete)
             Unknown = 0b00,
             UsbHost = 0b01,
             Adapter = 0b10,
             Otg = 0b11,
-        ]
+        ],
+        CHRG_STAT OFFSET(4) NUMBITS(2) [
+            NotCharging = 0b00,
+            PreCharge = 0b01,
+            FastCharging = 0b10,
+            ChargeTerminationDone = 0b11,
+        ],
+        DPM_STAT OFFSET(3) NUMBITS(1) [
+            NotDpm = 0,
+            /// VINDPM or IINDPM
+            Dpm = 1,
+        ],
+        PG_STAT OFFSET(2) NUMBITS(1) [
+            NotPowerGood = 0,
+            PowerGood = 1,
+        ],
+        THERM_STAT OFFSET(3) NUMBITS(1) [
+            Normal = 0,
+            ThermalRegulation = 1,
+        ],
+        VSYS_STAT OFFSET(3) NUMBITS(1) [
+            /// Not in VSYSMIN regulation (BAT > VSYSMIN)
+            NoRegulation = 0,
+            /// – In VSYSMIN regulation (BAT < VSYSMIN)
+            Regulation = 1,
+        ],
+    ],
+
+    /// Fault Register
+    pub REG09 [
+        WATCHDOG_FAULT OFFSET(7) NUMBITS(1) [
+            Normal = 0,
+            TimerExpired = 1,
+        ],
+        BOOST_FAULT OFFSET(6) NUMBITS(1) [
+            Normal = 0,
+            /// VBUS overloaded (OCP), or VBUS OVP in boost mode
+            Fault = 1,
+        ],
+        CHRG_FAULT OFFSET(4) NUMBITS(2) [
+            Normal = 0b00,
+            InputFault = 0b01,
+            ThermalShutdown = 0b10,
+            SafetyTimer = 0b11,
+        ],
+        BAT_FAULT OFFSET(3) NUMBITS(1) [],
+        NTC_FAULT OFFSET(0) NUMBITS(3) [
+            Normal = 0b000,
+            Cold = 0b101,
+            Hot = 0b110,
+        ],
+    ],
+
+    /// Vender / Part / Revision Status Register
+    pub REG0A [
+        PN OFFSET(3) NUMBITS(3) [
+            DEFAULT_PN = 0b101,
+        ],
+        TS_PROFILE OFFSET(2) NUMBITS(1) [],
+        DEV_REG OFFSET(0) NUMBITS(2) [],
     ]
 ];
 
-impl I2cRegister for InputSourceControl::Register {
+impl I2cRegister for REG00::Register {
     const ADDR: u8 = 0x00;
 }
 
-impl I2cRegister for PowerOnConfiguration::Register {
+impl I2cRegister for REG01::Register {
     const ADDR: u8 = 0x01;
 }
 
-// pub struct InputSourceControl {}
+impl I2cRegister for REG02::Register {
+    const ADDR: u8 = 0x02;
+}
 
-// impl RegisterLongName for InputSourceControl {}
+impl I2cRegister for REG03::Register {
+    const ADDR: u8 = 0x03;
+}
 
-// register_bitmasks!(u8, InputSourceControl, [
-//     HighZ OFFSET(7) NUMBITS(1) [
-//         Disabled = 0,
-//         Enabled = 1
-//     ],
-//     InputVoltageLimit OFFSET(3) NUMBITS(4) [
-//         VINDPM_3_88V = 0b0000,
-//         VINDPM_3_96V = 0b0001,
-//         VINDPM_4_04V = 0b0010,
-//         VINDPM_4_12V = 0b0011,
-//         VINDPM_4_20V = 0b0100,
-//         VINDPM_4_28V = 0b0101,
-//         VINDPM_4_36V = 0b0110,
-//         VINDPM_4_44V = 0b0111,
-//         VINDPM_4_52V = 0b1000,
-//         VINDPM_4_60V = 0b1001,
-//         VINDPM_4_68V = 0b1010,
-//         VINDPM_4_76V = 0b1011,
-//         VINDPM_4_84V = 0b1100,
-//         VINDPM_4_92V = 0b1101,
-//         VINDPM_5_00V = 0b1110,
-//         VINDPM_5_08V = 0b1111,
-//     ],
-//     InputCurrentLimit OFFSET(0) NUMBITS(3) [
-//         IINLIM_100mA = 0b000,
-//         IINLIM_150mA = 0b001,
-//         IINLIM_500mA = 0b010,
-//         IINLIM_900mA = 0b011,
-//         IINLIM_1200mA = 0b100,
-//         IINLIM_1500mA = 0b101,
-//         IINLIM_2000mA = 0b110,
-//         IINLIM_3000mA = 0b111,
-//     ]
-// ]);
+impl I2cRegister for REG04::Register {
+    const ADDR: u8 = 0x04;
+}
 
+impl I2cRegister for REG05::Register {
+    const ADDR: u8 = 0x05;
+}
 
+impl I2cRegister for REG06::Register {
+    const ADDR: u8 = 0x06;
+}
+
+impl I2cRegister for REG07::Register {
+    const ADDR: u8 = 0x07;
+}
+
+impl I2cRegister for REG08::Register {
+    const ADDR: u8 = 0x08;
+}
+
+impl I2cRegister for REG09::Register {
+    const ADDR: u8 = 0x09;
+}
+
+impl I2cRegister for REG0A::Register {
+    const ADDR: u8 = 0x0A;
+}
